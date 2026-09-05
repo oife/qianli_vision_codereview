11f013bbe94faa6e441e290c28be78896ced6bc3
kepler <3827200344@qq.com>
2026-09-06 00:39:38 +0800

    修正nis逻辑并修改伪nees判断阈值

--- tools/extended_kalman_filter/extended_kalman_filter.cpp
+++ tools/extended_kalman_filter/extended_kalman_filter.cpp
@@ -47,7 +47,14 @@ Eigen::VectorXd ExtendedKalmanFilter::update(
   std::function<Eigen::VectorXd(const Eigen::VectorXd &, const Eigen::VectorXd &)> z_subtract)
 {
   Eigen::VectorXd x_prior = x;
-  Eigen::MatrixXd K = P * H.transpose() * (H * P * H.transpose() + R).inverse();
+  Eigen::MatrixXd P_prior = P;
+  Eigen::MatrixXd S = H * P_prior * H.transpose() + R;
+
+  // NIS = residual^T * S^-1 * residual
+  Eigen::VectorXd residual = z_subtract(z, h(x));
+  double nis = residual.transpose() * S.inverse() * residual;
+
+  Eigen::MatrixXd K = P * H.transpose() * S.inverse();
 
   // Stable Compution of the Posterior Covariance
   // https://github.com/rlabbe/Kalman-and-Bayesian-Filters-in-Python/blob/master/07-Kalman-Filter-Math.ipynb
@@ -55,16 +62,11 @@ Eigen::VectorXd ExtendedKalmanFilter::update(
 
   x = x_add(x, K * z_subtract(z, h(x)));
 
-  /// 卡方检验
-  Eigen::VectorXd residual = z_subtract(z, h(x));
-  // 新增检验
-  Eigen::MatrixXd S = H * P * H.transpose() + R;
-  double nis = residual.transpose() * S.inverse() * residual;
   double nees = (x - x_prior).transpose() * P.inverse() * (x - x_prior);
 
   // 卡方检验阈值（自由度=4，取置信水平95%）
-  constexpr double nis_threshold = 0.711;
-  constexpr double nees_threshold = 0.711;
+  constexpr double nis_threshold = 9.488;
+  constexpr double nees_threshold = 19.725;
 
   if (nis > nis_threshold) nis_count_++, data["nis_fail"] = 1;
   if (nees > nees_threshold) nees_count_++, data["nees_fail"] = 1;
